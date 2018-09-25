259e7748aaf6cdef341243c85b39162a8f720e69
0K-Jeff <37250380+0K-Jeff@users.noreply.github.com>
2018-09-24 21:15:26 -0400

    Further updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,4 +23,28 @@ export class AppComponent {
     }
   }
 
+  public timerclass() {
+
+    const ticker = setInterval(function() {
+      // set variables
+      const timerfinish = new Date('Dec 25, 2018').getTime();
+      const currenttime = new Date().getTime();
+      const difference = timerfinish - currenttime;
+      const daysleft = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const hoursleft = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutesleft = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const secondsleft = Math.floor((difference % (1000 * 60)) / 1000);
+
+      // render
+      document.getElementById('timerdisplay').innerHTML =
+      'Submit by: ' + daysleft + 'd ' + hoursleft + 'h ' + minutesleft + 'm ' + secondsleft + 's ';
+
+      // display expiration
+      if (difference < 0) {
+        clearInterval(ticker);
+        document.getElementById('timerdisplay').innerHTML = 'Times up!';
+      }
+    }, 1000);
+  }
+
 }
