83c266e9268eacd6d3bd43805873d9b75ad3c7ac
0K-Jeff <37250380+0K-Jeff@users.noreply.github.com>
2018-09-27 11:10:45 -0400

    directory restructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule
