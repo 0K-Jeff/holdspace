0c8a62fc339876fb11a346fe4da4e557d7edd82b
0K-Jeff <37250380+0K-Jeff@users.noreply.github.com>
2018-11-01 18:24:33 -0400

    table setup

--- src/app/disposal-list/disposal-list.component.ts
+++ src/app/disposal-list/disposal-list.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
+
+export interface DisposalTransactionListItem {
+  date: string;
+  facility: string;
+  state: string;
+}
+
+// Dummy data - TODO replace with fetch methods using REST API
+
+const TRANSACTION_DATA: DisposalTransactionListItem[] = [
+  {date: '10/12/19', facility: 'fort belvoir', state: 'Installation Work'},
+  {date: '9/7/12', facility: 'fort knox', state: 'HQ Review'},
+  {date: '11/11/11', facility: 'fort knox', state: 'Installation Return'},
+  {date: '1/1/11', facility: 'fort belvoir', state: 'HQ Work'},
+  {date: '10/12/19', facility: 'fort belvoir', state: 'Installation Work'},
+  {date: '9/7/12', facility: 'fort knox', state: 'HQ Review'},
+  {date: '11/11/11', facility: 'fort knox', state: 'HQ Review'},
+  {date: '1/1/11', facility: 'fort belvoir', state: 'HQ Work'},
+  {date: '10/12/19', facility: 'fort belvoir', state: 'Installation Return'},
+  {date: '9/7/12', facility: 'fort knox', state: 'Installation Work'},
+  {date: '11/11/11', facility: 'fort knox', state: 'Installation Return'},
+  {date: '1/1/11', facility: 'fort belvoir', state: 'HQ Work'},
+];
 
 @Component({
   selector: 'app-disposal-list',
   templateUrl: './disposal-list.component.html',
-  styleUrls: ['./disposal-list.component.css']
+  styleUrls: ['./disposal-list.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class DisposalListComponent implements OnInit {
+  displayedColumns: string[] = ['actions', 'date', 'facility', 'state'];
+  dataSource = new MatTableDataSource<DisposalTransactionListItem>(TRANSACTION_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor() { }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
 }
--- src/app/material-comp/material-comp.module.ts
+++ src/app/material-comp/material-comp.module.ts
@@ -3,18 +3,27 @@ import { CommonModule } from '@angular/common';
 import { MatNativeDateModule } from '@angular/material';
 import { MatDatepickerModule } from '@angular/material';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   imports: [
     CommonModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatRadioModule
+    MatRadioModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   exports: [
     MatDatepickerModule,
     MatNativeDateModule,
-    MatRadioModule
+    MatRadioModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   declarations: []
 })
