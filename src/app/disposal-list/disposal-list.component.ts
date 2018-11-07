028b2f97998f6da8d8d19977c3e5ccfc25277646
0K-Jeff <37250380+0K-Jeff@users.noreply.github.com>
2018-11-06 20:34:53 -0500

    Setup commented functions

--- src/app/disposal-list/disposal-list.component.ts
+++ src/app/disposal-list/disposal-list.component.ts
@@ -2,26 +2,32 @@ import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
 
 export interface DisposalTransactionListItem {
-  date: string;
+  date: Date;
   facility: string;
   state: string;
 }
 
 // Dummy data - TODO replace with fetch methods using REST API
 
+const dataTransactionPacket = '{ "serverPacket" : [' +
+'{"dateString" : "2018-11-25" , "facility" : "fort belvoir" , "state" : "Installation Work"},' +
+'{"dateString" : "2015-03-25" , "facility" : "fort belvoir" , "state" : "Installation Work"} ]}';
+
+const dataBundle = JSON.parse(dataTransactionPacket);
+
 const TRANSACTION_DATA: DisposalTransactionListItem[] = [
-  {date: '10/12/19', facility: 'fort belvoir', state: 'Installation Work'},
-  {date: '9/7/12', facility: 'fort knox', state: 'HQ Review'},
-  {date: '11/11/11', facility: 'fort knox', state: 'Installation Return'},
-  {date: '1/1/11', facility: 'fort belvoir', state: 'HQ Work'},
-  {date: '10/12/19', facility: 'fort belvoir', state: 'Installation Work'},
-  {date: '9/7/12', facility: 'fort knox', state: 'HQ Review'},
-  {date: '11/11/11', facility: 'fort knox', state: 'HQ Review'},
-  {date: '1/1/11', facility: 'fort belvoir', state: 'HQ Work'},
-  {date: '10/12/19', facility: 'fort belvoir', state: 'Installation Return'},
-  {date: '9/7/12', facility: 'fort knox', state: 'Installation Work'},
-  {date: '11/11/11', facility: 'fort knox', state: 'Installation Return'},
-  {date: '1/1/11', facility: 'fort belvoir', state: 'HQ Work'},
+  {date: new Date (dataBundle.serverPacket[0].dateString), facility: dataBundle.serverPacket[0].facility, state: dataBundle.serverPacket[0].state},
+  {date: new Date ('2014-03-25'), facility: 'fort knox', state: 'HQ Review'},
+  {date: new Date ('2013-03-25'), facility: 'fort knox', state: 'Installation Return'},
+  {date: new Date ('2015-04-25'), facility: 'fort belvoir', state: 'HQ Work'},
+  {date: new Date ('2015-05-25'), facility: 'fort belvoir', state: 'Installation Work'},
+  {date: new Date ('2015-03-26'), facility: 'fort knox', state: 'HQ Review'},
+  {date: new Date ('2015-03-27'), facility: 'fort knox', state: 'HQ Review'},
+  {date: new Date ('2016-01-25'), facility: 'fort belvoir', state: 'HQ Work'},
+  {date: new Date ('2012-09-25'), facility: 'fort belvoir', state: 'Installation Return'},
+  {date: new Date ('2017-04-22'), facility: 'fort knox', state: 'Installation Work'},
+  {date: new Date ('2018-10-01'), facility: 'fort knox', state: 'Installation Return'},
+  {date: new Date ('2015-09-20'), facility: 'fort belvoir', state: 'HQ Work'},
 ];
 
 @Component({
@@ -38,7 +44,20 @@ export class DisposalListComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor() { }
+  constructor() {
+
+    // function disposalListArrayGenerator(translatedServerPacket: Object) {
+    //   const currentListPacket[];
+    //   for (let x = 0; x < translatedServerPacket.serverPacket.length(); x++) {
+    //     const transactionItem = {date: '', facility: '', state: ''};
+    //     currentListPacket.push(transactionItem);
+    //   }
+    //   return currentListPacket;
+    // }
+
+    // currentListPacket = disposalListArrayGenerator(dataTransactionPacket);
+
+  }
 
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
--- src/app/sessionStoreAPI.ts
+++ src/app/sessionStoreAPI.ts
@@ -1,20 +1,3 @@
 export class SessionStoreAPI {
     ticker: number;
 }
-
-export interface DisposalTransactionData {
-    date: Date;
-    facility: JSON;
-    sourceWeight: boolean;
-    sourceVolume: boolean;
-    volume: number;
-    conversionItem: JSON;
-    conversionRate: number;
-    pounds: number;
-    costRevenue: boolean;
-    weight: number;
-    costperpound: number;
-    tenant: JSON;
-    invoice: number;
-    localUse: JSON;
-}
