de365a7fae0753373672e2b923839adf5afdf90a
jkolodner <jkolodner@aspex.com>
2018-11-09 08:56:01 -0500

    updated form data

--- src/app/disposal-list/disposal-list.component.ts
+++ src/app/disposal-list/disposal-list.component.ts
@@ -4,31 +4,32 @@ import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
 export interface DisposalTransactionListItem {
   date: Date;
   facility: string;
-  state: string;
+  facilityType: string;
+  totalCost: number;
 }
 
 // Dummy data - TODO replace with fetch methods using REST API
 
 const dataTransactionPacket = '{ "serverPacket" : [' +
-'{"dateString" : "2018-11-25" , "facility" : "fort belvoir" , "state" : "Installation Work"},' +
-'{"dateString" : "2015-03-25" , "facility" : "fort belvoir" , "state" : "Installation Work"} ]}';
+'{"dateString" : "2016-10-26" , "facility" : "Trashbegone landfill and disposal" , "facilityType" : "D" , "totalCost" : "50.07"},' +
+'{"dateString" : "2016-10-25" , "facility" : "Camp Swampy incinerator" , "facilityType" : "W" , "totalCost" : "40.07"},' +
+'{"dateString" : "2017-07-05" , "facility" : "Camp Swampy incinerator" , "facilityType" : "W" , "totalCost" : "230.07"},' +
+'{"dateString" : "2018-01-15" , "facility" : "Camp Swampy incinerator" , "facilityType" : "W" , "totalCost" : "1111.07"},' +
+'{"dateString" : "2018-05-23" , "facility" : "Trashbegone landfill and disposal" , "facilityType" : "D" , "totalCost" : "40.47"},' +
+'{"dateString" : "2015-04-25" , "facility" : "Camp Swampy incinerator" , "facilityType" : "W" , "totalCost" : "423.07"},' +
+'{"dateString" : "2015-03-15" , "facility" : "Trashbegone landfill and disposal" , "facilityType" : "D" , "totalCost" : "300.57"} ]}';
 
 const dataBundle = JSON.parse(dataTransactionPacket);
 
-const TRANSACTION_DATA: DisposalTransactionListItem[] = [
-  {date: new Date (dataBundle.serverPacket[0].dateString), facility: dataBundle.serverPacket[0].facility, state: dataBundle.serverPacket[0].state},
-  {date: new Date ('2014-03-25'), facility: 'fort knox', state: 'HQ Review'},
-  {date: new Date ('2013-03-25'), facility: 'fort knox', state: 'Installation Return'},
-  {date: new Date ('2015-04-25'), facility: 'fort belvoir', state: 'HQ Work'},
-  {date: new Date ('2015-05-25'), facility: 'fort belvoir', state: 'Installation Work'},
-  {date: new Date ('2015-03-26'), facility: 'fort knox', state: 'HQ Review'},
-  {date: new Date ('2015-03-27'), facility: 'fort knox', state: 'HQ Review'},
-  {date: new Date ('2016-01-25'), facility: 'fort belvoir', state: 'HQ Work'},
-  {date: new Date ('2012-09-25'), facility: 'fort belvoir', state: 'Installation Return'},
-  {date: new Date ('2017-04-22'), facility: 'fort knox', state: 'Installation Work'},
-  {date: new Date ('2018-10-01'), facility: 'fort knox', state: 'Installation Return'},
-  {date: new Date ('2015-09-20'), facility: 'fort belvoir', state: 'HQ Work'},
-];
+// tslint:disable-next-line:prefer-const
+let TRANSACTION_DATA: DisposalTransactionListItem[] = [];
+
+for (let iterated = 0; iterated < dataBundle.serverPacket.length; iterated++) {
+  const arraySlot = {date: new Date(dataBundle.serverPacket[iterated].dateString), facility: dataBundle.serverPacket[iterated].facility,
+    facilityType: dataBundle.serverPacket[iterated].facilityType, totalCost: dataBundle.serverPacket[iterated].totalCost};
+
+    TRANSACTION_DATA.push(arraySlot);
+}
 
 @Component({
   selector: 'app-disposal-list',
@@ -38,7 +39,7 @@ const TRANSACTION_DATA: DisposalTransactionListItem[] = [
 })
 
 export class DisposalListComponent implements OnInit {
-  displayedColumns: string[] = ['actions', 'date', 'facility', 'state'];
+  displayedColumns: string[] = ['actions', 'date', 'facility', 'facilityType', 'totalCost'];
   dataSource = new MatTableDataSource<DisposalTransactionListItem>(TRANSACTION_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
